628b4cefc0db5f532e8f197edc29d3debdd1eddc
Christiaan Wagner <12860909+RedStop@users.noreply.github.com>
2022-10-02 19:05:38 +0200

    Added two functions.

--- JsonPropertyMerger.py
+++ JsonPropertyMerger.py
@@ -4,6 +4,8 @@
 
 import json
 import exiftool
+import datetime
+from typing import Any, Union, Optional, List, Dict
 
 
 class JsonPropertyMerger:
@@ -17,14 +19,51 @@ class JsonPropertyMerger:
             with open(jsonFile, mode="r") as file:
                 data = json.load(file)
         except Exception as e:
-            print("Failed to open file:", jsonFile, " Error:", e)
+            print("Failed to open file:", jsonFile, "Error:", e)
 
         if (data is not None):
             print("Data extracted:", data)
 
         return data
 
+    @staticmethod
+    def ExtractMetaData(file: Union[str, List[str]] = ""):
+        metadata = None
+        try:
+            with exiftool.ExifToolHelper() as et:
+                metadata = et.get_metadata(file)
+        except Exception as e:
+            print("Failed to extract metadata from file:", file, "Error:", e)
+
+        if (metadata is not None):
+            print(metadata)
+
+        if (type(file) == "str" and len(metadata) == 1):
+            return metadata[0]
+        else:
+            return metadata
+
+    @staticmethod
+    def UpdateImageMetaDataWithJson(imagefile: str, googleMetadata: json):
+        description = googleMetadata.get("description")
+        photoTakenTime = googleMetadata.get("photoTakenTime")
+        if (description is not None and len(description) > 0):
+            print("Description:", description)
+
+        if (photoTakenTime is not None):
+            photoTakenTimeEpoch = photoTakenTime.get("timestamp")
+            if (photoTakenTimeEpoch is not None):
+                print("PhotoTakenTimeEpoch:", photoTakenTimeEpoch)
+
 
 if __name__ == "__main__":
-    JsonPropertyMerger.ExtracJsonData("TestPhotos/IMG_20220102_094708117.jpg.json")
-    JsonPropertyMerger.ExtracJsonData("TestPhotos/IMG_20220102_094728921.jpg.json")
+    json = JsonPropertyMerger.ExtracJsonData("TestPhotos/IMG_20220102_094708117.jpg.json")
+    # JsonPropertyMerger.ExtracJsonData("TestPhotos/IMG_20220102_094728921.jpg.json")
+
+    JsonPropertyMerger.UpdateImageMetaDataWithJson("", json)
+
+    # JsonPropertyMerger.ExtractMetaData(["TestPhotos/IMG_20220102_094708117.jpg", "TestPhotos/IMG_20221001_091508974.jpg"])
+
+    JsonPropertyMerger.ExtractMetaData("TestPhotos/IMG_20220102_094728921.jpg")
+
+    # JsonPropertyMerger.ExtractMetaData(["TestPhotos/IMG_20220102_094728921.jpg", "TestPhotos/IMG_20220102_094708117.jpg"])
